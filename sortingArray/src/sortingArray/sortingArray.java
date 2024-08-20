ed4f46c1d2b79b1c50893b928bd1554814342e51
laura <you@example.com>
2024-08-20 12:52:46 -0500

    hallo :D!

---
+++ sortingArray/src/sortingArray/sortingArray.java
@@ -0,0 +1,15 @@
+package sortingArray;
+import java.util.Arrays;
+public class sortingArray {
+
+	public static void main(String[] args) {
+		
+	int [] myNum = {14 , 2 , 8 , 0};
+	
+	Arrays.sort (myNum);	
+	
+	System.out.println(Arrays.toString(myNum));
+	}
+	
+	
+}
